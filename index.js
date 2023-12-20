be27321256a4566e3c7141b1c2b460ee27981e98
Phil Wekullo <philwekullo@gmail.com>
2023-12-20 09:20:47 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    // Convert name to lowercase
+    const lowerCaseName = name.toLowerCase();
+   
+    // Filter the drivers array to get matching names
+    const matchingDrivers = drivers.filter(driver => {
+       // Convert driver name to lowercase
+       const lowerCaseDriver = driver.toLowerCase();
+   
+       // Check if the driver name includes the search name
+       return lowerCaseDriver.includes(lowerCaseName);
+    });
+   
+    // Return the matching list of drivers
+    return matchingDrivers;
+   }
+
+   function fuzzyMatch(drivers, name) {
+    // Convert name to lowercase
+    const lowerCaseName = name.toLowerCase();
+   
+    // Filter the drivers array to get matching names
+    const matchingDrivers = drivers.filter(driver => {
+       // Convert driver name to lowercase
+       const lowerCaseDriver = driver.toLowerCase();
+   
+       // Check if the driver name starts with the search name
+       return lowerCaseDriver.startsWith(lowerCaseName);
+    });
+   
+    // Return the matching list of drivers
+    return matchingDrivers;
+   }
+
+   function matchName(drivers, name) {
+    // Convert name to lowercase
+    const lowerCaseName = name.toLowerCase();
+   
+    // Filter the drivers array to get matching names
+    const matchingDrivers = drivers.filter(driver => {
+       // Convert driver name to lowercase
+       const lowerCaseDriver = driver.name.toLowerCase();
+   
+       // Check if the driver name matches the search name
+       return lowerCaseDriver === lowerCaseName;
+    });
+   
+    // Return the matching list of drivers
+    return matchingDrivers;
+   }
